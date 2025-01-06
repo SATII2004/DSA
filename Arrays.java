aee2cb88d65adbd3a33683a67ec09b924ad39732
N SATISH <162682521+SATII2004@users.noreply.github.com>
2025-01-06 23:14:22 +0530

    .

---
+++ Arrays.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Arrays
+{
+    public static void main(String[] args)
+    {
+        int[] marks = new int[3];
+        marks[0]=97; 
+        marks[1]=99;
+        marks[2]=89;
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+        System.out.println("//////////////////////////////////////////");
+        for(int i=0;i<3;i++)
+        { 
+            System.out.println(marks[i]);
+        }
+
+    }
+}
+
