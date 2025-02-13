576d9a3c4dcf4a6946e7600b79e3571c341e6824
N SATISH <162682521+SATII2004@users.noreply.github.com>
2025-02-13 23:37:28 +0530

    .

---
+++ Bubblesort.java
@@ -0,0 +1,30 @@
+
+public class Bubblesort {
+    public static void main(String[] args)
+    {
+        int arr[]={7,9,3,1,4};
+        int n= arr.length;
+        //bubble sort
+
+        for(int i=0;i<n-1;i++)
+        {
+            for(int j=0;j<n-i-1;j++)
+            {
+                if(arr[j]>arr[j+1])
+                {
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+                
+
+            }
+        }
+        for(int i=0;i<n;i++)
+    {
+        System.out.println( arr[i]);
+    }
+
+    }
+    
+}
